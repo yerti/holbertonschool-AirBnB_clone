01f853ac7d738e4cf398fc0a040c6c8cced0ebe3
yerti <yerti0818@gmail.com>
2023-08-02 16:04:37 -0700

    Add file because I modified the comments

--- models/base_model.py
+++ models/base_model.py
@@ -7,15 +7,14 @@ import datetime
 
 
 class BaseModel:
-    """The BaseModel class of future classes"""
     def __init__(self):
-
+        """We declare the public attributes"""
         self.id = str(uuid.uuid4())
         self.created_at = datetime.datetime.now()
         self.updated_at = self.created_at
 
     def __str__(self):
-        """String representation of the BaseModel class"""
+        """Returns us in chain"""
         return f"[BaseModel] ({self.id}) {self.__dict__}"
 
     def save(self):
@@ -23,7 +22,7 @@ class BaseModel:
         self.updated_at = datetime.datetime.now()
 
     def to_dict(self):
-        """Updates the attribute 'updated_at' with the current datetime"""
+        """Returns a dictionary containing all keys/values"""
         new_dict = self.__dict__.copy()
         new_dict["created_at"] = self.created_at.isoformat()
         new_dict["updated_at"] = self.updated_at.isoformat()
