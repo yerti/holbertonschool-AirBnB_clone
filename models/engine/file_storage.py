5277450864de9588b170fa960a610e5504034ffb
yerti <yerti0818@gmail.com>
2023-08-04 15:31:03 -0700

    modify the code to make my code work correctly

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,15 +8,16 @@ from models.base_model import BaseModel
 
 class FileStorage:
 
-        __file_path = "file.json"
-        __objects = {}
+
+    __file_path = "file.json"
+    __objects = {}
 
     def all(self):
         return self.__objects
 
     def new(self, obj):
-        key = "{}.{}".format(obj.__class__.__name__, obj.id)
-        self.__objects[key] = obj
+        key = "{}.{}".format(obj.__class__.__name__,obj.id)
+        FileStorage.__objects[key] = obj
 
     def save(self):
         objts_dict = {}
@@ -24,17 +25,17 @@ class FileStorage:
         for key, obj in self.__objects.items():
             objts_dict[key] = obj.to_dict()
 
-        with open(self.__file_path, "w", encoding='utf-8') as file:
+        with open(self.__file_path, 'w', encoding='utf-8') as file:
             json.dump(objts_dict, file)
 
     def reload(self):
-        if os.path.exists(self.__file_path):
-            with open(self.__file_path, 'r', encoding='utf-8') as file_a:
-                alm = file_a.read()
-                dictionary = json.loads(alm)
-                for key, value in dictionary.items():
-                    value = dictionary[key]
+        if os.path.exists(FileStorage.__file_path):
+            with open(self.__file_path, 'r', encoding="utf-8") as f:
+                alm = f.read()
+                dictr = json.loads(alm)
+                for key, value in dictr.items():
+                    value = dictr[key]
                     obj = eval(value['__class__'])(**value)
-                    self.__objects[key] = obj
+                    FileStorage.__objects[key] = obj
         else:
             pass
