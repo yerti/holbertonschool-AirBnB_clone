e830a0e470f36b7dfd529782f2390d38e7c21036
yerti <yerti0818@gmail.com>
2023-08-05 19:08:35 -0700

    Modify the file to add comments

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
-
+"""The FileStorage class handles serialization
+and deserialization of objects in JSON format."""
 
 import json
 import os
 from models.base_model import BaseModel
+from models.user import User
 
 
 class FileStorage:
 
-
+    """creating private attributes"""
     __file_path = "file.json"
     __objects = {}
 
@@ -16,10 +18,11 @@ class FileStorage:
         return self.__objects
 
     def new(self, obj):
-        key = "{}.{}".format(obj.__class__.__name__,obj.id)
+        key = "{}.{}".format(obj.__class__.__name__, obj.id)
         FileStorage.__objects[key] = obj
 
     def save(self):
+        """Is in charge of making the serrealization of the objects"""
         objts_dict = {}
 
         for key, obj in self.__objects.items():
@@ -29,6 +32,7 @@ class FileStorage:
             json.dump(objts_dict, file)
 
     def reload(self):
+        """Takes care of deserializing the objects stored in the json file"""
         if os.path.exists(FileStorage.__file_path):
             with open(self.__file_path, 'r', encoding="utf-8") as f:
                 alm = f.read()
