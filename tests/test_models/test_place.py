76ad464592b37d73fba92c4717c70f12a3fefa15
yerti <yerti0818@gmail.com>
2023-08-06 17:51:37 -0700

    modify the code to make the test work

--- tests/test_models/test_place.py
+++ tests/test_models/test_place.py
@@ -10,17 +10,17 @@ class TestPlace(unittest.TestCase):
     def test_attributes(self):
         place = Place()
 
-        self.assertTrue(hasattr(place, 'city_id'))
-        self.assertTrue(hasattr(place, 'user_id'))
-        self.assertTrue(hasattr(place, 'name'))
-        self.assertTrue(hasattr(place, 'description'))
-        self.assertTrue(hasattr(place, 'number_rooms'))
-        self.assertTrue(hasattr(place, 'number_bathrooms'))
-        self.assertTrue(hasattr(place, 'max_guest'))
-        self.assertTrue(hasattr(place, 'price_by_night'))
-        self.assertTrue(hasattr(place, 'latitude'))
-        self.assertTrue(hasattr(place, 'longitude'))
-        self.assertTrue(hasattr(place, 'amenity_ids'))
+        self.assertEqual(place.city_id, "")
+        self.assertEqual(place.user_id, "")
+        self.assertEqual(place.name, "")
+        self.assertEqual(place.description, "")
+        self.assertEqual(place.number_rooms, "")
+        self.assertEqual(place.number_bathrooms, "")
+        self.assertEqual(place.max_guest, "")
+        self.assertEqual(place.price_by_night, "")
+        self.assertEqual(place.latitude, "")
+        self.assertEqual(place.longitude, "")
+        self.assertEqual(place.amenity_ids, "")
 
 
 if __name__ == '__main__':
