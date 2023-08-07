dcd6156d8b6a31d95d7f315328f8db5e73675e1a
yerti <yerti0818@gmail.com>
2023-08-06 17:31:46 -0700

    adding the file to create the test of the place class

---
+++ tests/test_models/test_place.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+
+import unittest
+from models.place import Place
+
+
+class TestPlace(unittest.TestCase):
+
+    def test_attributes(self):
+        place = Place()
+
+        self.assertTrue(hasattr(place, 'city_id'))
+        self.assertTrue(hasattr(place, 'user_id'))
+        self.assertTrue(hasattr(place, 'name'))
+        self.assertTrue(hasattr(place, 'description'))
+        self.assertTrue(hasattr(place, 'number_rooms'))
+        self.assertTrue(hasattr(place, 'number_bathrooms'))
+        self.assertTrue(hasattr(place, 'max_guest'))
+        self.assertTrue(hasattr(place, 'price_by_night'))
+        self.assertTrue(hasattr(place, 'latitude'))
+        self.assertTrue(hasattr(place, 'longitude'))
+        self.assertTrue(hasattr(place, 'amenity_ids'))
+
+
+if __name__ == '__main__':
+    unittest.main()
