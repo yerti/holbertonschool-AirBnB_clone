7e1d244c0a5db535ebe4502cb24d33b19b9d4b6b
yerti <yerti0818@gmail.com>
2023-08-06 18:34:58 -0700

    modify the file so that it imports the BaseModel class

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -2,8 +2,8 @@
 """We will create a test of the BaseModel class"""
 
 
-import datetime
 import unittest
+from datetime import datetime
 from models.base_model import BaseModel
 
 
@@ -11,43 +11,39 @@ class TestBaseModel(unittest.TestCase):
 
     """Configure common test environment"""
     def setUp(self):
-        obj = BaseModel()
+        self.obj = BaseModel()
 
     """Tests each attribute"""
     def test_attributes(self):
-        obj = BaseModel()
-        self.assertTrue(hasattr(obj, 'id'))
-        self.assertTrue(hasattr(obj, 'created_at'))
-        self.assertTrue(hasattr(obj, 'updated_at'))
+        self.assertTrue(hasattr(self.obj, 'id'))
+        self.assertTrue(hasattr(self.obj, 'created_at'))
+        self.assertTrue(hasattr(self.obj, 'updated_at'))
 
     """Tests eachs __str__"""
     def test_str(self):
-        obj = BaseModel()
-        obj_str = str(obj)
-        out_str = "[BaseModel] ({}) {}".format(obj.id, obj.__dict__)
+        obj_str = str(self.obj)
+        out_str = "[BaseModel] ({}) {}".format(self.obj.id, self.obj.__dict__)
         self.assertEqual(obj_str, out_str)
 
     """check if it updates correctly after calling the save method"""
     def test_save(self):
-        obj = BaseModel()
-        obj_updated_at1 = obj.updated_at
+        obj_updated1 = self.obj.updated_at
 
-        obj.save()
-        obj_updated_at2 = obj.updated_at
+        self.obj.save()
+        obj_updated2 = self.obj.updated_at
 
-        self.assertNotEqual(obj_updated_at1, obj_updated_at2)
+        self.assertNotEqual(obj_updated1, obj_updated2)
 
 
     """check if the dictionary is created with the attributes"""
     def test_to_dict(self):
-        obj = BaseModel()
-        obj_dict = obj.to_dict()
+        obj_dict = self.obj.to_dict()
         self.assertTrue(isinstance(obj_dict, dict))
         self.assertEqual(obj_dict['__class__'], 'BaseModel')
         self.assertTrue('id' in obj_dict)
         self.assertTrue('created_at' in obj_dict)
         self.assertTrue('updated_at' in obj_dict)
-        self.assertNotEqual(obj_dict, obj.__dict__)
+        self.assertNotEqual(obj_dict, self.obj.__dict__)
 
 
 if __name__ == '__main__':
