f6f4acaebd0cf6310dd8bcf3310e292d332dd70d
yerti <yerti0818@gmail.com>
2023-08-06 17:52:13 -0700

    modify the code to make the test work

--- tests/test_models/test_review.py
+++ tests/test_models/test_review.py
@@ -10,9 +10,9 @@ class TestReview(unittest.TestCase):
     def test_attributes(self):
         review = Review()
 
-        self.assertTrue(hasattr(review, 'place_id'))
-        self.assertTrue(hasattr(review, 'user_id'))
-        self.assertTrue(hasattr(review, 'text'))
+        self.assertEqual(review.place_id, "")
+        self.assertEqual(review.user_id, "")
+        self.assertEqual(review.text, "")
 
 
 if __name__ == '__main__':
